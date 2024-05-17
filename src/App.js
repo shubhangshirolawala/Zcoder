d5b9a8e81aa92aac329f37f7f925ceb50a4dcd3f
shubhangshirolawala <158447779+shubhangshirolawala@users.noreply.github.com>
2024-05-17 15:19:20 +0530

    LandingPage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+
+import LandingPage from './pages/LandingPage';
+function App() {
+  return (
+    
+  <LandingPage/>
+  );
+}
+
+export default App;
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Main from '../components/Main'
+import Footer from '../components/Footer'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Main/>
+        <Footer/>
+      
+    </div>
+  )
+}
+
+export default LandingPage
