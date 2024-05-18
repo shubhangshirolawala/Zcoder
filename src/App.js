64dacda80c5d5e44da79afd0119fa533b534493b
nirvedh-harpal <harpalnirvedh@gmail.com>
2024-05-18 13:00:26 +0530

    update_routes

--- src/App.js
+++ src/App.js
@@ -5,18 +5,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import LandingPage from "./pages/LandingPage";
 import Login from "./components/login/Login";
-import SignIn from "./components/login/SignIn";
-import SignUp from "./components/login/SignUp";
-import ForgotPassword from "./components/login/ForgotPassword";
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -32,23 +32,6 @@ const Login = () => {
                 "section-headings " +
                 (option === 1 ? "sign-in" : option === 2 ? "sign-up" : "forgot")
               }>
-              {/* <ul className="options">
-              <li
-                className={option === 1 ? "active" : ""}
-                onClick={() => setOption(1)}>
-                Don't have an account ? Get started here
-              </li>
-              <li
-                className={option === 2 ? "active" : ""}
-                onClick={() => setOption(2)}>
-                Sign up
-              </li>
-              <li
-                className={option === 3 ? "active" : ""}
-                onClick={() => setOption(3)}>
-                Forgot
-              </li>
-            </ul> */}
               <div className="options bottomline">
                 <span onClick={() => setOption(2)}>
                   Don't have an account ? Get started here
@@ -60,17 +43,6 @@ const Login = () => {
               </div>
             </div>
           </section>
-          {/* <header>
-          <div
-            className={
-              "header-headings " +
-              (option === 1 ? "sign-in" : option === 2 ? "sign-up" : "forgot")
-            }>
-            <span>Don't have an account? Get started here</span>
-            <span>Already a member? Log In here</span>
-            <span>Reset your password</span>
-          </div>
-        </header> */}
         </div>
       </div>
     </div>
