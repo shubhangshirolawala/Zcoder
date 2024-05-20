e631c325d591f614259ef1535b02fa986a853fd6
devpatel008 <118624283+devpatel008@users.noreply.github.com>
2024-05-20 13:41:23 +0530

    updated Backend

--- server/app.js
+++ server/app.js
@@ -1,38 +1,39 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
-const cors = require('cors');
+const cors = require("cors");
 
-
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // error handler
 
-const connectDB = require('./db/connect');
-const authenticateUser = require('./middleware/authentication');
+const connectDB = require("./db/connect");
+const authenticateUser = require("./middleware/authentication");
 
 // routers
-const authRouter = require('./routes/auth');
+const authRouter = require("./routes/auth");
 const questionsRouter = require("./routes/questions");
 const commnetsRouter = require("./routes/comments");
+const userRouter = require("./routes/user");
 
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
-// const questions = require('./models/questions');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const questions = require("./models/questions");
 
 app.use(express.json());
 // extra packages
 app.use(cors());
 
 // routes
-app.get('/', (req, res) => {
-  res.send('Zcoder api');
+app.get("/", (req, res) => {
+  res.send("Zcoder api");
 });
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/questions/',authenticateUser,questionsRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/questions/", authenticateUser, questionsRouter);
 app.use("/api/v1/comments", authenticateUser, commnetsRouter);
+app.use("/api/v1/user", authenticateUser, userRouter);
 // app.use('/api/v1/patient', authenticateUser, vitalsRouter);
 // app.use('/api/v1/doctor', authenticateUser, doctorRouter);
 
--- server/models/User.js
+++ server/models/User.js
@@ -27,6 +27,9 @@ const UserSchema = new mongoose.Schema({
     default:
       "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
   },
+  friends: {
+    type: [String],
+  },
 });
 
 UserSchema.pre("save", async function () {
