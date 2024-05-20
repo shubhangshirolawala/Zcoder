cdc295796568249205e300faa2cbdc05a554787c
devpatel008 <118624283+devpatel008@users.noreply.github.com>
2024-05-20 13:52:19 +0530

    User Route Error Resloved

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,10 +8,9 @@ const {
   getAllFriends,
 } = require("../controllers/user");
 router.get("/getUser", getUser);
-router.get("otherUser/:id", getOtherUser);
+router.get("/otherUser/:id", getOtherUser);
 router.patch("/addFriend/:id", addFriend);
 router.patch("/removeFriend/:id", removeFriend);
 router.get("/getAllFriends", getAllFriends);
-
 
 module.exports = router;
